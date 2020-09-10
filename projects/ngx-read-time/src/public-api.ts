5c0a7c712a1eaaaee4639315fb63addfc8e07de0
uzairkorai <uzairahmed097@gmail.com>
2020-09-11 02:55:33 +0500

    fix issue

---
+++ projects/ngx-read-time/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ngx-read-time
+ */
+
+export * from './lib/ngx-read-time.service';
+export * from './lib/ngx-read-time.component';
+export * from './lib/ngx-read-time.module';
