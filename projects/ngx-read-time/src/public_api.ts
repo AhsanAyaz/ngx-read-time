257492e6217bac295fc20eddfaa1d727fc9b3c42
uzairkorai <uzairahmed097@gmail.com>
2020-09-11 02:33:21 +0500

    Create library, and the demo

--- projects/ngx-read-time/src/public_api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of ngx-read-time
- */
-
-export * from './lib/ngx-read-time.service';
-export * from './lib/ngx-read-time.directive';
-export * from './lib/ngx-read-time-options';
-export * from './lib/ngx-read-time.module';
